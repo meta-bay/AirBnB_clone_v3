6399cca381c10bbba6f0f450a04501783b73b705
Bruka25 <bkteshome862@gmail.com>
2024-04-01 00:28:35 +0300

    pycodestyle fix for the states object that can handle all restful apis

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""
+This is module states
+"""
 
 from flask import Flask, jsonify, request, abort
 from models import storage
 from models.state import State
 from api.v1.views import app_views
 
+
 @app_views.route('/states', methods=['GET'])
 def get_states():
     """Retrieves the list of all State objects"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
+
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
@@ -19,6 +24,7 @@ def get_state(state_id):
         return jsonify(state.to_dict())
     else:
         abort(404)
+
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
@@ -30,6 +36,7 @@ def delete_state(state_id):
         return jsonify({})
     else:
         abort(404)
+
 
 @app_views.route('/states', methods=['POST'])
 def create_state():
@@ -43,6 +50,7 @@ def create_state():
     storage.new(new_state)
     storage.save()
     return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
@@ -58,4 +66,3 @@ def update_state(state_id):
             setattr(state, key, value)
     storage.save()
     return jsonify(state.to_dict()), 200
-
