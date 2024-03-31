46d0f57bb506e3ac85c435d6656bfefa6e372a3f
meta-bay <bayiemeseret@gmail.com>
2024-03-31 17:15:47 +0300

    method to teardown

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,9 +9,13 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-
-
 app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_storage():
+    ''' closes the storage '''
+    storage.close()
 
 
 if __name__ == "__main__":
