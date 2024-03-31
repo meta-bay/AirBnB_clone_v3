2348e4fdf95ed398e5ac757f4639476e9970d817
Bruka25 <bkteshome862@gmail.com>
2024-04-01 00:24:43 +0300

    python state object that can handle all the restful api actions

---
+++ api/v1/views/states.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify, request, abort
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    """Retrieves the list of all State objects"""
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    """Retrieves a State object"""
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """Deletes a State object"""
+    state = storage.get(State, state_id)
+    if state:
+        storage.delete(state)
+        storage.save()
+        return jsonify({})
+    else:
+        abort(404)
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Creates a State"""
+    if not request.json:
+        abort(400, "Not a JSON")
+    if 'name' not in request.json:
+        abort(400, "Missing name")
+    data = request.get_json()
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """Updates a State object"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    if not request.json:
+        abort(400, "Not a JSON")
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
+
